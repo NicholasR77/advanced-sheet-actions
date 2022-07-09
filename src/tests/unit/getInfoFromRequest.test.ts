dda4f0f0fbb5ae846f34bd0124a3a4f77bf85446
NicholasR77 <nl7robbins@icloud.com>
2022-07-09 08:16:16 -0700

    Update getInfoFromRequest.test.ts

--- src/tests/unit/getInfoFromRequest.test.ts
+++ src/tests/unit/getInfoFromRequest.test.ts
@@ -5,6 +5,7 @@ const ownerName = 'test-owner';
 const repoName = 'test-repo';
 const branchName = 'test-branch';
 const branchString = 'string/string/test-branch'
+const sheetsWithPath = ['sheet1/path1/', 'sheet2/path1/path2', 'sheet3/path1/path2/path3'];
 
 describe('getInfoFromRequest', () => {
     beforeEach(() => {
@@ -13,13 +14,24 @@ describe('getInfoFromRequest', () => {
     });
 
     test('returns the repo, branch, and changes files', () => {
-        const fileList = 'sheet1/path1/, sheet2/path1/path2, sheet3/path1/path2/path3';
+        const fileList = 'sheet1/path1/,sheet2/path1/path2,sheet3/path1/path2/path3';
         const seperator = ',';
 
         const { repo, branch, files } = getInfoFromRequest(fileList, seperator)
 
         expect(repo).toEqual(repoName);
         expect(branch).toEqual(branchName);
-        expect(files[0]).toEqual('sheet1/path1/');
+        expect(files).toEqual(sheetsWithPath);
+    });
+
+    test('returns the repo, branch, and changes files with a different separator', () => {
+        const fileList = 'sheet1/path1/|sheet2/path1/path2|sheet3/path1/path2/path3';
+        const seperator = '|';
+
+        const { repo, branch, files } = getInfoFromRequest(fileList, seperator)
+
+        expect(repo).toEqual(repoName);
+        expect(branch).toEqual(branchName);
+        expect(files).toEqual(sheetsWithPath);
     });
 })
